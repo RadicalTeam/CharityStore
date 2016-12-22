70613d471ce3c39695df326899dc69fe2e1f64a1
Piao Luo <pluoaction@gmail.com>
2016-12-22 11:14:12 +0800

    submit form test

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -19,7 +19,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'app works!';
-        this.group = ["don't", "stop", "step"];
     }
     AppComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["G" /* Component */])({
@@ -31,7 +30,7 @@ var AppComponent = (function () {
     ], AppComponent);
     return AppComponent;
 }());
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/app.component.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/app.component.js.map
 
 /***/ },
 
@@ -58,7 +57,7 @@ webpackEmptyContext.id = 328;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(410);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(431);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app___ = __webpack_require__(430);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app___ = __webpack_require__(429);
 
 
 
@@ -68,7 +67,7 @@ if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment *
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__angular_core__["_23" /* enableProdMode */])();
 }
 __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_4__app___["a" /* AppModule */]);
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/main.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/main.js.map
 
 /***/ },
 
@@ -81,7 +80,7 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dyna
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(402);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(407);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(287);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__home_home_component__ = __webpack_require__(429);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__submit_form_submit_form_component__ = __webpack_require__(430);
 /* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return AppModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -105,7 +104,7 @@ var AppModule = (function () {
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["I" /* NgModule */])({
             declarations: [
                 __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_5__home_home_component__["a" /* HomeComponent */]
+                __WEBPACK_IMPORTED_MODULE_5__submit_form_submit_form_component__["a" /* SubmitFormComponent */],
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["b" /* BrowserModule */],
@@ -119,7 +118,7 @@ var AppModule = (function () {
     ], AppModule);
     return AppModule;
 }());
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/app.module.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/app.module.js.map
 
 /***/ },
 
@@ -127,8 +126,22 @@ var AppModule = (function () {
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_component__ = __webpack_require__(287);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(428);
+/* unused harmony namespace reexport */
+/* harmony namespace reexport (by used) */ __webpack_require__.d(exports, "a", function() { return __WEBPACK_IMPORTED_MODULE_1__app_module__["a"]; });
+
+
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/index.js.map
+
+/***/ },
+
+/***/ 430:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return HomeComponent; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return SubmitFormComponent; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -139,36 +152,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-var HomeComponent = (function () {
-    function HomeComponent() {
+var SubmitFormComponent = (function () {
+    function SubmitFormComponent() {
     }
-    HomeComponent.prototype.ngOnInit = function () {
+    SubmitFormComponent.prototype.ngOnInit = function () {
     };
-    HomeComponent = __decorate([
+    SubmitFormComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["G" /* Component */])({
-            selector: 'app-home',
+            selector: 'app-submit-form',
             template: __webpack_require__(589),
             styles: [__webpack_require__(587)]
         }), 
         __metadata('design:paramtypes', [])
-    ], HomeComponent);
-    return HomeComponent;
+    ], SubmitFormComponent);
+    return SubmitFormComponent;
 }());
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/home.component.js.map
-
-/***/ },
-
-/***/ 430:
-/***/ function(module, exports, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_component__ = __webpack_require__(287);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(428);
-/* unused harmony namespace reexport */
-/* harmony namespace reexport (by used) */ __webpack_require__.d(exports, "a", function() { return __WEBPACK_IMPORTED_MODULE_1__app_module__["a"]; });
-
-
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/index.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/submit-form.component.js.map
 
 /***/ },
 
@@ -184,7 +183,7 @@ var HomeComponent = (function () {
 var environment = {
     production: false
 };
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/environment.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/environment.js.map
 
 /***/ },
 
@@ -240,14 +239,14 @@ var environment = {
 
 
 
-//# sourceMappingURL=/Users/pluo/Desktop/quickStart/testCli/src/polyfills.js.map
+//# sourceMappingURL=/Users/pluo/Desktop/weChatDev/Fimiliarity/src/polyfills.js.map
 
 /***/ },
 
 /***/ 586:
 /***/ function(module, exports) {
 
-module.exports = "p{\n\tcolor:blue;\n\tfont-size:20px;\n}"
+module.exports = ""
 
 /***/ },
 
@@ -261,14 +260,14 @@ module.exports = ""
 /***/ 588:
 /***/ function(module, exports) {
 
-module.exports = "<h1>\n  {{title}}\n</h1>\n<p *ngFor='let word of group'>{{word}}</p>"
+module.exports = "<app-submit-form></app-submit-form>\n<!-- <h1>input</h1>\n<div class=\"weui-cells__title\">单选列表项</div>\n<div class=\"weui-cells weui-cells_radio\">\n    <label class=\"weui-cell weui-check__label\" for=\"x11\">\n        <div class=\"weui-cell__bd\">\n            <p>cell standard</p>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <input type=\"radio\" class=\"weui-check\" name=\"radio1\" id=\"x11\">\n            <span class=\"weui-icon-checked\"></span>\n        </div>\n    </label>\n    <label class=\"weui-cell weui-check__label\" for=\"x12\">\n\n        <div class=\"weui-cell__bd\">\n            <p>cell standard</p>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <input type=\"radio\" name=\"radio1\" class=\"weui-check\" id=\"x12\" checked=\"checked\">\n            <span class=\"weui-icon-checked\"></span>\n        </div>\n    </label>\n    <a href=\"javascript:void(0);\" class=\"weui-cell weui-cell_link\">\n        <div class=\"weui-cell__bd\">添加更多</div>\n    </a>\n</div>\n<div class=\"weui-cells__title\">复选列表项</div>\n<div class=\"weui-cells weui-cells_checkbox\">\n    <label class=\"weui-cell weui-check__label\" for=\"s11\">\n        <div class=\"weui-cell__hd\">\n            <input type=\"checkbox\" class=\"weui-check\" name=\"checkbox1\" id=\"s11\" checked=\"checked\">\n            <i class=\"weui-icon-checked\"></i>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <p>standard is dealt for u.</p>\n        </div>\n    </label>\n    <label class=\"weui-cell weui-check__label\" for=\"s12\">\n        <div class=\"weui-cell__hd\">\n            <input type=\"checkbox\" name=\"checkbox1\" class=\"weui-check\" id=\"s12\">\n            <i class=\"weui-icon-checked\"></i>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <p>standard is dealicient for u.</p>\n        </div>\n    </label>\n    <a href=\"javascript:void(0);\" class=\"weui-cell weui-cell_link\">\n        <div class=\"weui-cell__bd\">添加更多</div>\n    </a>\n</div>\n\n<div class=\"weui-cells__title\">表单</div>\n<div class=\"weui-cells weui-cells_form\">\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__hd\"><label class=\"weui-label\">qq</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" pattern=\"[0-9]*\" placeholder=\"请输入qq号\"/>\n        </div>\n    </div>\n    <div class=\"weui-cell weui-cell_vcode\">\n        <div class=\"weui-cell__hd\">\n            <label class=\"weui-label\">手机号</label>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"tel\" placeholder=\"请输入手机号\">\n        </div>\n        <div class=\"weui-cell__ft\">\n            <a href=\"javascript:;\" class=\"weui-vcode-btn\">获取验证码</a>\n        </div>\n    </div>\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__hd\"><label for=\"\" class=\"weui-label\">日期</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"date\" value=\"\"/>\n            </div>\n    </div>\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__hd\"><label for=\"\" class=\"weui-label\">时间</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"datetime-local\" value=\"\" placeholder=\"\"/>\n        </div>\n    </div>\n    <div class=\"weui-cell weui-cell_vcode\">\n        <div class=\"weui-cell__hd\"><label class=\"weui-label\">验证码</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" placeholder=\"请输入验证码\"/>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <img class=\"weui-vcode-img\" src=\"./images/vcode.jpg\" />\n        </div>\n    </div>\n</div>\n<div class=\"weui-cells__tips\">底部说明文字底部说明文字</div>\n\n<div class=\"weui-cells__title\">表单报错</div>\n<div class=\"weui-cells weui-cells_form\">\n    <div class=\"weui-cell weui-cell_warn\">\n        <div class=\"weui-cell__hd\"><label for=\"\" class=\"weui-label\">卡号</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" pattern=\"[0-9]*\" value=\"weui input error\" placeholder=\"请输入卡号\"/>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <i class=\"weui-icon-warn\"></i>\n        </div>\n    </div>\n</div>\n\n\n<div class=\"weui-cells__title\">开关</div>\n<div class=\"weui-cells weui-cells_form\">\n    <div class=\"weui-cell weui-cell_switch\">\n        <div class=\"weui-cell__bd\">标题文字</div>\n        <div class=\"weui-cell__ft\">\n            <input class=\"weui-switch\" type=\"checkbox\"/>\n        </div>\n    </div>\n</div>\n\n<div class=\"weui-cells__title\">文本框</div>\n<div class=\"weui-cells\">\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"text\" placeholder=\"请输入文本\"/>\n        </div>\n    </div>\n</div>\n\n<div class=\"weui-cells__title\">文本域</div>\n<div class=\"weui-cells weui-cells_form\">\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__bd\">\n            <textarea class=\"weui-textarea\" placeholder=\"请输入文本\" rows=\"3\"></textarea>\n            <div class=\"weui-textarea-counter\"><span>0</span>/200</div>\n        </div>\n    </div>\n</div>\n\n<div class=\"weui-cells__title\">选择</div>\n<div class=\"weui-cells\">\n\n    <div class=\"weui-cell weui-cell_select weui-cell_select-before\">\n        <div class=\"weui-cell__hd\">\n            <select class=\"weui-select\" name=\"select2\">\n                <option value=\"1\">+86</option>\n                <option value=\"2\">+80</option>\n                <option value=\"3\">+84</option>\n                <option value=\"4\">+87</option>\n            </select>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" pattern=\"[0-9]*\" placeholder=\"请输入号码\"/>\n        </div>\n    </div>\n</div>\n<div class=\"weui-cells__title\">选择</div>\n<div class=\"weui-cells\">\n    <div class=\"weui-cell weui-cell_select\">\n        <div class=\"weui-cell__bd\">\n            <select class=\"weui-select\" name=\"select1\">\n                <option selected=\"\" value=\"1\">微信号</option>\n                <option value=\"2\">QQ号</option>\n                <option value=\"3\">Email</option>\n            </select>\n        </div>\n    </div>\n    <div class=\"weui-cell weui-cell_select weui-cell_select-after\">\n        <div class=\"weui-cell__hd\">\n            <label for=\"\" class=\"weui-label\">国家/地区</label>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <select class=\"weui-select\" name=\"select2\">\n                <option value=\"1\">中国</option>\n                <option value=\"2\">美国</option>\n                <option value=\"3\">英国</option>\n            </select>\n        </div>\n    </div>\n</div>\n\n<label for=\"weuiAgree\" class=\"weui-agree\">\n    <input id=\"weuiAgree\" type=\"checkbox\" class=\"weui-agree__checkbox\">\n    <span class=\"weui-agree__text\">\n        阅读并同意<a href=\"javascript:void(0);\">《相关条款》</a>\n    </span>\n</label>\n\n<div class=\"weui-btn-area\">\n    <a class=\"weui-btn weui-btn_primary\" href=\"javascript:\" id=\"showTooltips\">确定</a>\n</div>\n -->"
 
 /***/ },
 
 /***/ 589:
 /***/ function(module, exports) {
 
-module.exports = "<p>\n  home works!\n</p>\n"
+module.exports = "<h1>商品信息</h1>\n<div class=\"weui-cells weui-cells_form\">\n\t<div class=\"weui-uploader\">\n\t    <div class=\"weui-uploader__hd\">\n\t        <p class=\"weui-uploader__title\">商品图片</p>\n\t        <div class=\"weui-uploader__info\">0/2</div>\n\t    </div>\n\t    <div class=\"weui-uploader__bd\">\n\t        <ul class=\"weui-uploader__files\" id=\"uploaderFiles\">\n\t            <li class=\"weui-uploader__file\" style=\"background-image:url(./images/pic_160.png)\"></li>  \n\t        </ul>\n\t        <div class=\"weui-uploader__input-box\">\n\t            <input id=\"uploaderInput\" class=\"weui-uploader__input\" type=\"file\" accept=\"image/*\" multiple />\n\t        </div>\n\t    </div>\n\t</div>\n\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__hd\"><label class=\"weui-label\">商品名称</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"text\" name=\"stuffName\" placeholder=\"请输入商品名称\"/>\n        </div>\n    </div>\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__hd\"><label class=\"weui-label\">商品标价</label></div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" name=\"stuffName\" placeholder=\"请输入商品价格\"/>\n        </div>\n    </div>\n\n \t<div class=\"weui-cell weui-cell_select weui-cell_select-after\">\n        <div class=\"weui-cell__hd\">\n            <label for=\"\" class=\"weui-label\">商品分类</label>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <select class=\"weui-select\" name=\"select2\">\n                <option value=\"1\">电子用品</option>\n                <option value=\"2\">衣物</option>\n                <option value=\"3\">生活用品</option>\n            </select>\n        </div>\n    </div>\n<div class=\"weui-cells__title\">商品描述</div>\n    <div class=\"weui-cell\">\n        <div class=\"weui-cell__bd\">\n            <textarea class=\"weui-textarea\" name=\"stuffDeatil\" placeholder=\"请输入商品描述\" rows=\"3\"></textarea>\n            <div class=\"weui-textarea-counter\"><span>0</span>/200</div>\n        </div>\n    </div>\n\n    <div class=\"weui-cells weui-cells_radio\">\n    <label class=\"weui-cell weui-check__label\" for=\"x11\">\n        <div class=\"weui-cell__bd\">\n            <p>是</p>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <input type=\"radio\" class=\"weui-check\" name=\"radio1\" id=\"x11\">\n            <span class=\"weui-icon-checked\"></span>\n        </div>\n    </label>\n    <label class=\"weui-cell weui-check__label\" for=\"x12\">\n\n        <div class=\"weui-cell__bd\">\n            <p>否</p>\n        </div>\n        <div class=\"weui-cell__ft\">\n            <input type=\"radio\" name=\"radio1\" class=\"weui-check\" id=\"x12\" checked=\"checked\">\n            <span class=\"weui-icon-checked\"></span>\n        </div>\n    </label>\n</div>\n\n     <div class=\"weui-cell weui-cell_select weui-cell_select-after\">\n        <div class=\"weui-cell__hd\">\n            <label for=\"\" class=\"weui-label\">捐赠组织</label>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <select class=\"weui-select\" name=\"select2\">\n                <option value=\"1\">壹</option>\n                <option value=\"2\">贰</option>\n                <option value=\"3\">叁</option>\n                <option value=\"4\">其他</option>\n            </select>\n        </div>\n    </div>\n    <span class=\"weui-agree__text\">\n        你的买卖所得将捐赠给你选择的组织，感谢你的捐赠！\n    </span>\n\n<div class=\"weui-cells\">\n\n    <div class=\"weui-cell weui-cell_select weui-cell_select-before\">\n        <div class=\"weui-cell__hd\">\n            <select class=\"weui-select\" name=\"select2\">\n                <option value=\"1\">+86</option>\n                <option value=\"2\">+80</option>\n                <option value=\"3\">+84</option>\n                <option value=\"4\">+87</option>\n            </select>\n        </div>\n        <div class=\"weui-cell__bd\">\n            <input class=\"weui-input\" type=\"number\" pattern=\"[0-9]*\" placeholder=\"请您输入号码，以便买家联系\"/>\n        </div>\n    </div>\n</div>\n\n<div class=\"weui-btn-area\">\n    <a class=\"weui-btn weui-btn_primary\" href=\"javascript:\" id=\"showTooltips\">确定</a>\n</div>\n\n  \n</div>\n"
 
 /***/ },
 
